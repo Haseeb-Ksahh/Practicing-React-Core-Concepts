a650808da68362106857802a110eba0d3009ca9f
ksahh <haseebshaikh25ks@gmail.com>
2023-07-08 06:04:35 +0530

    In this updated code, class names have been added to each component, and the corresponding CSS styles have been provided for each class name. This will ensure that the CSS styles are applied correctly to the respective components.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import { LOGOURL } from './component/config';
 
 // Logo component
 const Logo = () => (
-  <img src={LOGOURL} alt="zomatoLogo" style={{ height: '21px' }} />
+  <img src={LOGOURL} alt="zomatoLogo" className="logo" />
 );
 
 // Header component
@@ -21,8 +21,13 @@ function Header() {
     <div className="header">
       <Logo />
       <div className="navbar">
-        <input type="text" onChange={handleSearchChange} value={searchText} />
-        <ul>
+        <input
+          type="text"
+          onChange={handleSearchChange}
+          value={searchText}
+          className="search-input"
+        />
+        <ul className="navbar-menu">
           <li>Home</li>
           <li>About</li>
           <li>Cart</li>
@@ -34,15 +39,15 @@ function Header() {
 
 // RestaurantCard component
 const RestaurantCard = () => (
-  <div>
+  <div className="restaurant-card">
     <img
       src="https://b.zmtcdn.com/data/pictures/chains/5/19748365/c3cc93d05fdd7d2a1cfa3aa91f917fff_o2_featured_v2.jpg?output-format=webp"
       alt="restaurantPic"
-      style={{ height: '150px' }}
+      className="restaurant-card-image"
     />
-    <h3>Winners Pizza - Fresh dough</h3>
-    <h4>Pizza, Italian, Dessert</h4>
-    <h3>Rating 3.3</h3>
+    <h3 className="restaurant-card-title">Winners Pizza - Fresh dough</h3>
+    <h4 className="restaurant-card-category">Pizza, Italian, Dessert</h4>
+    <h3 className="restaurant-card-rating">Rating 3.3</h3>
   </div>
 );
 
@@ -54,7 +59,7 @@ const Body = () => (
 );
 
 // Footer component
-const Footer = () => <h4>Footer</h4>;
+const Footer = () => <h4 className="footer">Footer</h4>;
 
 // AppLayout component
 const AppLayout = () => (
