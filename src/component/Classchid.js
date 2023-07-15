6bd0c925c8588120655e6a03796ff3b076a5eede
ksahh <haseebshaikh25ks@gmail.com>
2023-07-15 18:19:04 +0530

    Trying new things

--- src/component/Classchid.js
+++ src/component/Classchid.js
@@ -1,34 +1,32 @@
-import { Component } from "react";
+import React, { Component } from "react";
 
-class Child extends Component{
+class Child extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Child constructor");
+  }
 
-    constructor(props){
+//   componentDidMount() {
+//     this.interval = setInterval(() => {
+//       // Perform some action repeatedly at the specified interval
+//       console.log("Interval callback");
+//     }, 1000); // Interval of 1 second (1000 milliseconds)
+//   }
 
-        const {user} = props;
+//   componentWillUnmount() {
+//     // Clear the interval when the component is unmounted
+//     clearInterval(this.interval);
+//   }
 
-        super(props)
-        this.state = {
-            childuser : user
-        }
-        console.log("Child constructor");
-        console.log(this.state.childuser);
-    }
-
-    componentDidMount(){
-        console.log("Child component did mount");
-    }
-
-    componentDidUpdate(){
-        console.log("Child component did update");
-    }
-
-    render(){
-        console.log("Child render");
-        return(
-            <h1>Its child components {this.state.childuser.name} and email id is {this.state.childuser.email}</h1>
-        )
-    }
-
+  render() {
+    console.log("Child render");
+    return (
+      <h1>
+        It's the child component. Name: {this.props.user.name}, Email:{" "}
+        {this.props.user.email}
+      </h1>
+    );
+  }
 }
 
 export default Child;
--- src/component/Classcom1.js
+++ src/component/Classcom1.js
@@ -1,32 +1,44 @@
-import { Component } from "react";
+import React, { Component } from "react";
 import Child from "./Classchid";
 
-class Parent extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            user : {
-                name : "haseeb shaikh",
-                email : "haseebshaikh25ks@gmail.com"
-            }
-        }    
-        console.log("Parent constructor component");
-    }
+class Parent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        name: "haseeb shaikh",
+        email: "haseebshaikh25ks@gmail.com",
+      },
+    };
+    console.log("Parent constructor component");
+  }
 
-    componentDidMount(){
-        console.log("Parent component did mount");
-    }
+  componentDidMount() {
+    console.log("Parent component did mount");
+  }
 
-    componentDidUpdate(){
-        console.log("Parent component did update");
-    }
+  componentDidUpdate() {
+    console.log("Parent component did update");
+  }
 
-    render(){
-        console.log("Parent render component");
-        return(
-            <Child user={this.state.user}/>
-        )
-    }
+  handleChange = () => {
+    this.setState({
+      user: {
+        ...this.state.user,
+        name: "Ksahh shaikh",
+      },
+    });
+  };
+
+  render() {
+    console.log("Parent render component");
+    return (
+      <>
+        <button onClick={this.handleChange}>Change name to Ksahh</button>
+        <Child user={this.state.user} />
+      </>
+    );
+  }
 }
 
 export default Parent;
